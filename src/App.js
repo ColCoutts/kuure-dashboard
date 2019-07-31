ca66ad5b0fd62ae715c3bacb1ccb9aca5d947336
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-07-31 16:14:50 -0700

    created dashboard page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/header-component';
+import Dashboard from './pages/dashboard/dashboard-component';
+
 
 function App() {
   return (
     <div className="App">
     <Header />
+    <Switch>
+      <Route exact path='/' component={Dashboard}/>
+    </Switch>
     </div>
   );
 }
---
+++ src/pages/dashboard/dashboard-component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './dashboard.styles.scss';
+
+const Dashboard = () => (
+  <div className='dashboard'>
+    HiHIHIHIHIH
+  </div>
+)
+
+export default Dashboard;
