3188bd07e82ac7cb7507edb06b6653788f3149e2
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-08-01 11:12:13 -0700

    created sales container

---
+++ src/components/sales/sales-component.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './sales.styles.scss';
+
+class Sales extends Component {
+  constructor() {
+    super();
+    this.state = {
+      metrics: ''
+    }
+  }
+
+  render() {
+    return(
+      <div className='sales-container'>
+      OH HAI THERE!
+      </div>
+    )
+  }
+}
+
+export default Sales;
--- src/pages/dashboard/dashboard.component.jsx
+++ src/pages/dashboard/dashboard.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import './dashboard.styles.scss';
 import Collabs from '../../components/collabs/collabs-component';
+import Sales from '../../components/sales/sales-component';
 
 const Dashboard = () => (
   <div className='dashboard'>
+    <Sales />
     <Collabs />
   </div>
 )
