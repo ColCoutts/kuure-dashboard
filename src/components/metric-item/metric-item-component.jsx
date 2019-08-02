89e8f366a364e6479692ca087f3dec6f5509f2d1
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-08-02 14:27:55 -0700

    added new props for metrics

--- src/components/metric-item/metric-item-component.jsx
+++ src/components/metric-item/metric-item-component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './metric-item.styles.scss';
 
-const MetricItem = ({ title, metric }) => (
-  <div className='metric-item'>
+const MetricItem = ({ title, metric, color }) => (
+  <div className={`${color} metric-item`}>
     <h1>{metric}</h1>
     <h2>{title}</h2>
   </div>
--- src/components/sales/sales.data.js
+++ src/components/sales/sales.data.js
@@ -2,17 +2,20 @@ const metrics = [
   {
     title: 'Total Views',
     metric: '18,398',
-    id: 1
+    id: 1,
+    color: 'purple'
   },
   {
     title: 'Total Sales',
     metric: '$1,043.23',
-    id: 2
+    id: 2,
+    color: 'green'
   },
   {
     title: 'Commissions',
     metric: '$845.25',
-    id: 3
+    id: 3,
+    color: 'red'
   }
 ]
 
