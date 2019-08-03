626d1e94b8c7c31120fb6a6ccbb20e857b72f9cd
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-08-02 17:11:49 -0700

    Updates

---
+++ precache-manifest.fc6511b8552e39fbd4feb3d28b16b099.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e49b18ef1c663e328523a3a7dd491d56",
+    "url": "/kuure-dashboard/index.html"
+  },
+  {
+    "revision": "41d7a111cbb3ecfb95a0",
+    "url": "/kuure-dashboard/static/css/main.491729ac.chunk.css"
+  },
+  {
+    "revision": "e0bb6623a59ffb5e2a74",
+    "url": "/kuure-dashboard/static/js/2.7a8d6ead.chunk.js"
+  },
+  {
+    "revision": "41d7a111cbb3ecfb95a0",
+    "url": "/kuure-dashboard/static/js/main.7ed07884.chunk.js"
+  },
+  {
+    "revision": "4010d7df4bbfa2e61407",
+    "url": "/kuure-dashboard/static/js/runtime~main.8c125c7d.js"
+  },
+  {
+    "revision": "92fb10993b959547ac16ae663bee9a3f",
+    "url": "/kuure-dashboard/static/media/Kuure-logo.92fb1099.svg"
+  }
+]);
